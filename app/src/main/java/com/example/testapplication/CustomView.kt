210c7a1eee497a5c2eb3eaeba6d7be32f77001cc
KienHT <kienhantrung@gmail.com>
2021-01-26 01:10:48 +0700

    xxx

--- app/src/main/java/com/example/testapplication/CustomView.kt
+++ app/src/main/java/com/example/testapplication/CustomView.kt
@@ -1,60 +1,126 @@
 package com.example.testapplication
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import java.io.File
+import androidx.annotation.WorkerThread
+import androidx.core.content.ContextCompat
+import androidx.core.view.doOnPreDraw
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class CustomView constructor(
-        context: Context,
-        attrs: AttributeSet
-) : View(context, attrs) {
+class CustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
 
-    private var myPositionBitmap: Bitmap = BitmapFactory.decodeResource(
-            context.resources,
-            R.drawable.ic_my_position, BitmapFactory.Options()
-            .apply {
-                inScaled = false
-            }
-    )
+    private val positionPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        .apply {
+            isDither = true
+            isFilterBitmap = true
+        }
 
-    private val selectedLocation: SelectLocationObject
+    private val positionBitmap: Bitmap
+
+    private val density: Int
+        get() = when (context.resources.displayMetrics.density) {
+            0.75f -> DisplayMetrics.DENSITY_LOW
+            1f -> DisplayMetrics.DENSITY_MEDIUM
+            1.5f -> DisplayMetrics.DENSITY_HIGH
+            2f -> DisplayMetrics.DENSITY_XHIGH
+            3f -> DisplayMetrics.DENSITY_XXHIGH
+            4f -> DisplayMetrics.DENSITY_XXXHIGH
+            else -> DisplayMetrics.DENSITY_DEFAULT
+        }
+
+    private var positionX = -1f
+    private var positionY = -1f
+
     init {
-        selectedLocation =
-                SelectLocationObject(myPositionBitmap)
+        val source = ContextCompat.getDrawable(context, R.drawable.ic_my_position)
+        val bitmap = if (source is BitmapDrawable) {
+            source.bitmap
+        } else {
+            BitmapFactory.decodeResource(
+                context.resources,
+                R.drawable.ic_my_position,
+                BitmapFactory.Options()
+                    .apply {
+                        inScaled = false
+                    }
+            )
+        }
+
+        positionBitmap = Bitmap.createScaledBitmap(
+            bitmap,
+            bitmap.width,
+            bitmap.height,
+            true
+        )
+
+        doOnPreDraw {
+            drawPosition(
+                (width / 2 - positionBitmap.width / 2).toFloat(),
+                (height / 2 - positionBitmap.height / 2).toFloat()
+            )
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
-        canvas.apply {
-//            imageBackground.draw(this)
-            selectedLocation.draw(this)
+        if (positionX >= 0 && positionY >= 0) {
+            canvas.drawBitmap(
+                positionBitmap,
+                positionX,
+                positionY,
+                positionPaint
+            )
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.action == MotionEvent.ACTION_UP) {
-            selectedLocation.showAt(event.x, event.y)
-            invalidate()
+            drawPosition(event.x, event.y)
         }
         return true
     }
 
-    fun captureRoomBitMap(): Bitmap {
-        val frameBitmap = Bitmap.createBitmap(
+    @WorkerThread
+    fun capture(): Bitmap? = try {
+        Bitmap.createBitmap(
             measuredWidth,
             measuredHeight,
             Bitmap.Config.ARGB_8888
-        )
-        Canvas(frameBitmap).apply /*.withTranslation(myPositionBitmap.width*1.0f/2, myPositionBitmap.height*1.0f/2)*/ {
-            this@CustomView.draw(this)
+        ).also {
+            Canvas(it)
+                .apply {
+                    density = this@CustomView.density
+                    drawColor(Color.GREEN)
+                }.let { canvas ->
+                    draw(canvas)
+                }
         }
-        return frameBitmap
+    } catch (e: Exception) {
+        Log.e(TAG, "capture: ", e)
+        null
+    }
+
+    private fun drawPosition(x: Float, y: Float) {
+        positionX = x
+        positionY = y
+        invalidate()
+    }
+
+    companion object {
+        private val TAG: String = CustomView::class.java.simpleName
     }
 }
 
--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -1,51 +1,37 @@
 package com.example.testapplication
 
-import android.content.Context
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
-import java.io.FileOutputStream
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.view.doOnPreDraw
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button_test)
-        val view = findViewById<CustomView>(R.id.custom_view)
-        button.setOnClickListener {
-            val bitmap = view.captureRoomBitMap()
-            getScreenshotPath()
-                .takeUnless { it.isEmpty() }
-                ?.let { bitmap.saveToFile(it, true) }
-                ?.absolutePath
+        val customView = findViewById<CustomView>(R.id.custom_view)
+        val buttonCapture = findViewById<Button>(R.id.button_capture)
+        val imageView = findViewById<AppCompatImageView>(R.id.image_view)
+
+        buttonCapture.setOnClickListener {
+            lifecycleScope.launch {
+                val bitmap = withContext(Dispatchers.IO) {
+                    customView.capture()
+                }
+                if (bitmap != null) {
+                    imageView.setImageBitmap(bitmap)
+                }
+            }
+        }
+
+        customView.doOnPreDraw {
+            buttonCapture.performClick()
         }
     }
-}
-
-fun Bitmap.saveToFile(path: String, isRecycle: Boolean = false): File? {
-    return try {
-        val file = File(path)
-        file.createNewFile()
-        FileOutputStream(file).use { out -> this.compress(Bitmap.CompressFormat.PNG, 100, out) }
-        file
-    } catch (e: Exception) {
-        null
-    } finally {
-        if (isRecycle && !isRecycled) {
-            recycle()
-        }
-    }
-}
-
-fun Context.getScreenshotPath(): String {
-    val folderPath = "${cacheDir}/screenshot"
-    val folder = File(folderPath)
-    if (!folder.exists()) {
-        folder.mkdirs()
-    }
-    return "${folderPath}/${System.currentTimeMillis()}.png"
 }
